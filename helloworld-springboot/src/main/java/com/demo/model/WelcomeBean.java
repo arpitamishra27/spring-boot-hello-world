19b1c53843ad999a7870a67738bb28ef78f62329
Arpita Mishra <63522967+arpitamishra27@users.noreply.github.com>
2024-01-23 00:30:06 -0500

    Uploaded the project

---
+++ helloworld-springboot/src/main/java/com/demo/model/WelcomeBean.java
@@ -0,0 +1,16 @@
+package com.demo.model;
+
+
+public class WelcomeBean {
+	private String message;
+
+	public WelcomeBean(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+	
+}
